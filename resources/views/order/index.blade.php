4e21668be8d774528e1cd6fde45d4d87b787ce7b
sandhityap <made.sandhitya.pramana@atlasbeachfest.com>
2025-05-24 11:46:25 +0800

    Update menu order add filter

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -51,6 +51,59 @@
     @endif
 
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div>
+            <form method="GET" action="{{ route('order.index') }}">
+                <div class="row flex justify-end items-center">
+                    <div class="relative rounded-md shadow-sm">
+                        <label for="meja" class="block text-sm font-medium text-gray-700">Kode Order</label>
+                        <input type="text" name="kode" placeholder="Cari ..." value="{{ request('kode') }}"
+                            class="block w-full pl-4 pr-10 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                        <div class="absolute inset-y-0 right-0 flex items-center mt-5 pr-3 pointer-events-none">
+                            <svg class="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <div class="col-auto px-2 py-3">
+                        <label for="meja" class="block text-sm font-medium text-gray-700">Pilih Meja</label>
+                        <select name="meja" id="meja"
+                            class="w-45 px-3 py-2 mt-1 text-gray-700 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-150">
+                            <option value="">Semua Meja</option>
+                            @foreach ($mejas as $meja)
+                                <option value="{{ $meja->id }}"
+                                    {{ request('meja') == $meja->id ? 'selected' : '' }}>
+                                    {{ $meja->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="col-auto px-2 py-3">
+                        <label for="date" class="block text-sm font-medium text-gray-700">Tanggal Order</label>
+                        <input type="date" name="date" id="date" value="{{ $date }}"
+                            class="mt-1 block w-50 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                    </div>
+
+                    <div class="col-auto px-2 py-3 mt-5">
+                        <button type="submit"
+                            class="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                            Filter
+                        </button>
+                    </div>
+
+                    <div class="col-auto px-2 py-3 mt-5">
+                        <a href="{{ route('order.index') }}"
+                            class="px-6 py-3 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                            Reset
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
+
         <div class="p-6 bg-white border-b border-gray-200">
             <div class="overflow-x-auto">
                 <table class="min-w-full divide-y divide-gray-200">
